8a803d252cdb48ef733f97fc93f3b4ca41049c57
Isaac <isaac@izak.amsterdam>
2024-01-17 15:28:29 +0100

    :wrench: Add long description to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import setup
 
 setup(name='ml_wac',
       version='0.1',
-      description='What the module does',
+      description='A Machine Learning Web-based Attack Classifier to detect and identify LFI, RFI, SQLI, and XSS attacks using the request paths',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
       url='https://github.com/izak0s/ml_wac/',
       author='Jord & Isaac',
       author_email='ml_wac@izak.dev',
